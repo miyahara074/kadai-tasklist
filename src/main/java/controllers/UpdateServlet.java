af0ca0f50581b652395f75ad60c929441ebe6a3b
user074.dounyu <user.user074.dounyu2025@tmns-techacademy.jp>
2025-06-09 14:35:20 +0900

    UpdateServlet.javaを作成

--- src/main/java/controllers/UpdateServlet.java
+++ src/main/java/controllers/UpdateServlet.java
@@ -18,7 +18,7 @@ import utils.DBUtil;
  */
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
